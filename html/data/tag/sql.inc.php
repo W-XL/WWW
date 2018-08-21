23bae936983fe855adcc8176982c59d3760b60f1
shenhaiyu <1536975466@qq.com>
2018-08-21 13:47:04 +0800

    框架搭建

---
+++ html/data/tag/sql.inc.php
@@ -0,0 +1,20 @@
+<?php
+global $sqltag;
+// phpwind8数据库连接配置
+// ------------------------------------------------------------------------
+$sqltag['console']['dbhost'] = 'localhost';
+$sqltag['console']['dbname'] = 'console';
+$sqltag['console']['dbuser'] = 'root';
+$sqltag['console']['dbpwd'] = 'Qazx1472580@cake';
+$sqltag['console']['dbprefix'] = '';
+$sqltag['console']['dblanguage'] = 'utf8';
+
+// phpb2b数据库连接配置
+// ------------------------------------------------------------------------
+$sqltag['phpb2b']['dbhost'] = 'localhost';
+$sqltag['phpb2b']['dbname'] = 'cake';
+$sqltag['phpb2b']['dbuser'] = 'root';
+$sqltag['phpb2b']['dbpwd'] = 'Qazx1472580@cake';
+$sqltag['phpb2b']['dbprefix'] = '';
+$sqltag['phpb2b']['dblanguage'] = 'utf8';
+?>
---
+++ html/html/course/copy_new/course.js
@@ -0,0 +1,14 @@
+
+function goto()
+{
+    //快餐通
+    // window.location.href="http://z1-pcok6.kuaishangkf.com/bs/im.htm?cas=116885___253445&fi=119315";
+
+    //53
+    window.location.href="https://tb.53kf.com/code/client/10176347/1";
+}
+function contact()
+{
+    window.location.href="https://tb.53kf.com/code/client/10176347/1";
+    // window.location.href="http://z1-pcok6.kuaishangkf.com/bs/im.htm?cas=116885___253445&fi=119315";
+}
